000b42e83b4ffda71b02769c3dda08e5eee37de9
zhenyaFlower <onlymirage@live.ru>
2022-09-27 18:52:02 +0300

    Added 2 lines "System out print with messages"

--- app/src/main/java/HomeWork.java
+++ app/src/main/java/HomeWork.java
@@ -1,3 +1,6 @@
 public class HomeWork {
-
+    public static void main(String[] args) {
+        System.out.println("GitHub is working ))");
+        System.out.println("Maybe can I do anything else?");
+    }
 }
