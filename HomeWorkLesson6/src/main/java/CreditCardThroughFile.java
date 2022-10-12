c54467fea93a403bda7fcaff53df1337cfdc6715
zhenyaFlower <onlymirage@live.ru>
2022-10-12 22:41:18 +0300

    HomeWork Lesson 6, Credit Card through file.

--- HomeWorkLesson6/src/main/java/CreditCardThroughFile.java
+++ HomeWorkLesson6/src/main/java/CreditCardThroughFile.java
@@ -4,69 +4,83 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class CreditCardThroughFile extends Write{
+public class CreditCardThroughFile extends Write {
 
     public static void main(String[] args) throws IOException {
         Write file = new Write();
-        file.setBalance(7);
-        file.setSecondBalance(3);
-        file.setSecondBalance(4);
-        file.setSecondCard(8);
+        file.array = new int[][]{{556455488, 10}, {254565545, 10}, {564564656, 10}};
+        file.setBalance(3);
+        file.setSecondBalance(11);
+        file.setThirdBalance(5);
         file.setSecondArray(file.secondArray);
         file.write();
+
+
     }
 }
 
 
 class Write {
-    int [][] array = new int[2][3];
-    int cardNumber=array[1][2];
-    int secondCard =array[1][0];
-    int balance = array[2][1];
-    int secondBalance = array[1][1];
-    int [][] secondArray=array;
+    int[][] array = new int[3][3];
+    int balance;
+    int secondBalance;
+    int thirdBalance;
+    int[][] secondArray = array;
+    String message = "You don't have enough money in a credit card № ";
+    public void setSecondArray(int[][] secondArray1) {
 
-    public void setCardNumber(int cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public void setSecondCard(int secondCard) {
-        this.secondCard = secondCard;
-    }
-
-    public void setSecondArray(int[][] secondArray) {
-        secondArray = new int[][]{{556455488, 10}, {254565545, 10}};;
-        secondArray[0][0]=cardNumber;
-        secondArray[1][0]=secondCard;
-        secondArray[0][1]=secondBalance;
-        secondArray[1][1]=balance;
-        this.secondArray = secondArray;
+        secondArray[0][1] = array[0][1] - getBalance();//balance
+        secondArray[1][0] = array[1][0];
+        secondArray[0][0] = array[0][0];
+        secondArray[1][1] = array[1][1] + getSecondBalance();//second balance
+        secondArray[2][0] = array[2][0];
+        secondArray[2][1] = array[2][1] + getThirdBalance();//third balance
+        this.secondArray = secondArray1;
     }
 
-    public void setSecondBalance(int bal) {
-        array[1][1]=secondBalance;
-        this.secondBalance = secondBalance+bal;
+    public int getBalance() {
+        return balance;
     }
 
-    public void setBalance(int newBal) {
-        array[0][1]=newBal;
-        if (newBal<=balance){
-        this.balance = balance-newBal;
+    public int getThirdBalance() {
+        return thirdBalance;
     }
-        else System.out.println("You don't have enough money");
+
+    public int getSecondBalance() {
+        return secondBalance;
     }
+
+    public void setThirdBalance(int balThird) {
+        this.thirdBalance = secondArray[2][1] + balThird;
+    }
+
+    public void setSecondBalance(int secBal) {
+        if (array[1][1] < secBal) {
+            System.out.println(message+array[1][0]);
+        } else
+            this.secondBalance = secondArray[1][1] - secBal;
+    }
+
+    public void setBalance(int firstBal) {
+        if (array[0][1] < firstBal) {
+            System.out.println(message+array[0][0]);
+        } else
+            this.balance = secondArray[0][1] + firstBal;
+    }
+
 
     void write() throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("HomeWorkLesson6/src/main/java/CreditCard.txt"));
-        for (int i =0;i< array.length;i++) {
-            for (int j = 0; j < array.length; j++) {
-                writer.write(String.valueOf(array[i][j]));
+        for (int i = 0; i < secondArray.length; i++) {
+            for (int j = 0; j < secondArray.length-1; j++) {
+                writer.write(String.valueOf(secondArray[i][j]));
                 writer.write(" ");
             }
             writer.write("\r\n");
         }
-            writer.flush();
-        }
+        writer.flush();
+
+    }
 }
 
 
